539be8dc20a6be141ec48649bd4d117a258d2398
OhSeungYoung <osy0056@khu.ac.kr>
2020-10-24 00:15:07 +0900

    feat: Add NavBar layout
    I determined NavBar position with flex. and I added a logo and search bar, but there is still a lot to implement.

--- src/components/base/NavBar/NavBar.js
+++ src/components/base/NavBar/NavBar.js
@@ -1,16 +1,59 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components';
 import SearchBar from './SearchBar'
+import { ReactComponent as AirbnbLogo } from '../../../svg/logo.svg'
 
+/**
+ * Components responsible for layout navigation bar
+ */
+
+const NavBlock = styled.div`
+    display: flex;
+    background: white;
+    height: 80px;
+    align-items: center;
+
+
+    .left-block, .right-block {
+        width: 466px;
+        height: 80px;
+    }
+
+    .left-block {
+        margin-left: 50px;
+    }
+
+    .center-block {
+        width: 348px;
+        height: 48px;
+    }
+`;
+
+const LogoWrapper = styled.svg`
+        fill: blue !important;
+        height: 80px !important;
+        width: 102px !important;
+        path {
+            fill: rgb(255, 56, 92) !important;
+        }
+`
 
 const NavBar = () => {
     return (
-        <div>
-            <div classname="logo-area">
-                <Link to="/">airbnb</Link>
+        <NavBlock>
+            <div className='left-block'>
+                <LogoWrapper>
+                    <AirbnbLogo />
+                </LogoWrapper>
             </div>
-            <SearchBar />
-        </div>
+            <div className='center-block'>
+                <SearchBar />
+            </div>
+            <div className='right-block'>
+
+            </div>
+        </NavBlock>
     )
 }
 
--- src/components/base/NavBar/SearchBar.js
+++ src/components/base/NavBar/SearchBar.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import styled from 'styled-components';
+
+const StyledSearch = styled.input`
+    padding: 0;
+    font-size: 1rem;
+    border-radius: 15px;
+    width: 300px !important;
+    height: 48px !important;
+`
 
 const SearchBar = () => {
     return (
-        <div>
-            SearchBar
-        </div>
+        <form>
+            <StyledSearch placeholder="검색 시작하기" />
+        </form>
     )
 }
 
 export default SearchBar
+
+
+
