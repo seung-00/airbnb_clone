db02fd35b575809baca02054c0221b5e516b2350
OhSeungYoung <osy0056@khu.ac.kr>
2020-11-01 20:55:44 +0900

    feat: Add useState to handle click event
    I used useState to handle click event Instead of Redux

--- src/components/base/NavBar/NabBar.js
+++ src/components/base/NavBar/NabBar.js
@@ -1,23 +1,47 @@
 /* eslint-disable react/prop-types */
 import React, { useState } from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import ButtonBlock from '@/components/search/ButtonBlock';
 import MainBlock from '@/components/search/MainBlock';
 import MenuBlock from '@/components/search/MenuBlock';
-import SearchForm from '@/container/search/SearchForm';
 import { respondTo } from '@/components/utils/mixins';
 import { ReactComponent as LogoSvg } from '@/svg/logo.svg';
+import SearchBlock from '@/components/search/SearchBlock';
 
 /**
  * Components responsible for layout navigation bar
  */
 
-const NavBlock = styled.div`
-  display: flex;
-  background: white;
+const NavHeader = styled.header`
+  position: fixed;
+  left: 0px;
+  top: 0px;
   height: 80px;
-  align-items: center;
+  width: 100%;
+  background-color: white;
+  z-index: 100;
+
+  ${(props) =>
+    props.selectedTab &&
+    css`
+      &::after {
+        position: absolute;
+        content: '';
+        left: 0px;
+        width: 100%;
+        height: 100%;
+        background-color: white;
+        z-index: 0;
+      }
+    `}
+`;
+
+const NavWraper = styled.div`
+  display: flex;
+  height: auto;
   text-align: left;
+  position: relative;
+  z-index: 1;
   ${respondTo.mobile`
         padding-left: 24px;
         padding-right: 24px;
@@ -30,15 +54,20 @@ const NavBlock = styled.div`
         padding-left: 80px;
         padding-right: 80px;
     `}
-    
-    div {
+
+  div {
     // fix flex-basis size for outer block
     &.left-block {
-      flex: 1 0 140px;
+      flex: 1 0 130px;
+      display: flex;
+      align-items: center;
+      height: 80px;
     }
     &.right-block {
-      flex: 1 0 140px;
+      flex: 1 0 130px;
       display: flex;
+      align-items: center;
+      height: 80px;
       justify-content: flex-end;
     }
     &.center-block {
@@ -58,26 +87,34 @@ const LogoWrapper = styled.div`
 
 const NavBar = () => {
   const [isSearching, setSearching] = useState(false);
-
+  const [selectedTab, setSelectedTab] = useState('');
   return (
-    <NavBlock>
-      <div className="left-block">
-        <LogoWrapper>
-          <LogoSvg />
-        </LogoWrapper>
-      </div>
-      <div className="center-block">
-        {isSearching ? (
-          <SearchForm />
-        ) : (
-          <ButtonBlock setSearching={setSearching} />
-        )}
-      </div>
-      <div className="right-block">
-        <MainBlock />
-        <MenuBlock />
-      </div>
-    </NavBlock>
+    <NavHeader selectedTab={selectedTab}>
+      <NavWraper>
+        <div className="left-block">
+          <LogoWrapper>
+            <LogoSvg />
+          </LogoWrapper>
+        </div>
+        <div className="center-block">
+          {isSearching ? (
+            <SearchBlock
+              selectedTab={selectedTab}
+              setSelectedTab={setSelectedTab}
+            />
+          ) : (
+            <ButtonBlock
+              setSelectedTab={setSelectedTab}
+              setSearching={setSearching}
+            />
+          )}
+        </div>
+        <div className="right-block">
+          <MainBlock />
+          <MenuBlock />
+        </div>
+      </NavWraper>
+    </NavHeader>
   );
 };
 
